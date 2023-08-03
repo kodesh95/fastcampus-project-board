58124e7597b3782a386bb7ba78b3b6092ca385cf
kodesh95 <kodesh95@gamil.com>
2023-08-03 09:31:54 +0900

    #7-스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술:
    
    *spring web
    * actuator
    *devtools
    *lombok

---
+++ f-p-b/src/test/java/com/fastcampus/fpb/FPBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FPBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
